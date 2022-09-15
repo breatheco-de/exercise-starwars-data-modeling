b8ce7706c86e83b09357b70742a19fdce49b01ad
tomasherman77 <tomasxherman@gmail.com>
2022-09-15 18:48:59 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique = True)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Favourites(Base):
+    __tablename__ = 'favourites'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_fav_planet = Column(Integer, ForeignKey('planets.id'))
+    user_fav_characters = Column(Integer, ForeignKey('characters.id'))
+
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table planets.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable = False)
+    favourites = relationship(Favourites)
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table character.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable = False)
+    favourites = relationship(Favourites)    
 
     def to_dict(self):
         return {}
