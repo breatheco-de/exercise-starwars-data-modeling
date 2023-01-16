93354caf3adb443127f5c6e9339480f32133d57d
irenecervillavalle <irenecervillavalle@gmail.com>
2023-01-16 20:22:24 +0000

    adding Starwars Data Modelling

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id_usuario = Column(Integer, primary_key=True)
+    nombre = Column(String(20), nullable=False)
+    apellido = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(12), nullable=False)
+    fecha_suscripcion = Column(String(12))
+    id_personaje = Column(Integer, ForeignKey("personaje.id_personaje"))
+    personaje = relationship("Personaje")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_personaje= Column(Integer, primary_key=True)
+    nombre = Column(String(50), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Fav_person_plan(Base):
+    __tablename__ = 'fav_person_plan'
+    id_fav = Column(Integer,primary_key=True)
+    id_usuario = Column(Integer, ForeignKey("usuario.id_usuario"))
+    id_personaje = Column(Integer, ForeignKey("personaje.id_personaje"))
+    id_planeta = Column(Integer, ForeignKey("planeta.id_planeta"))
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id_planeta = Column(Integer,primary_key=True)
+    nombre = Column(String(20), nullable=False)
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
