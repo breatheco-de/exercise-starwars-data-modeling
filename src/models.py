fce26aa7e5693a0fed7275737a66fb13def62f94
gustavomm19 <gustavomm199@gmail.com>
2023-01-29 19:46:40 +0000

    doing ex

--- src/models.py
+++ src/models.py
@@ -1,12 +1,24 @@
 import os
 import sys
+import uuid
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import ARRAY, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+class Film(Base):
+    __tablename__ = 'film'
+    
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    person = relationship('FilmPerson', uselist=True, backref='film')
 
 class Person(Base):
     __tablename__ = 'person'
@@ -14,6 +26,28 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    birth_year = Column(String(250))
+    eye_color = Column(String(250))
+    films = relationship('FilmPerson', uselist=True, backref='person')
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    height = Column(Integer)
+    homeworld = Column(String(250))
+    mass = Column(Integer)
+    skin_color = Column(String(250))
+    created = Column(TIMESTAMP, server_default=func.now(), nullable=False)
+    edited = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp(), nullable=False)
+    species = Column(ARRAY(String(250)))
+    starships = Column(ARRAY(String(250)))
+    url = Column(String(250))
+    starships = Column(ARRAY(String(250)))
+
+class FilmPerson(Base):
+    __tablename__ = 'person_movies'
+
+    person_id = Column(UUID(as_uuid=True), ForeignKey('person.id'))
+    movie_id = Column(UUID(as_uuid=True), ForeignKey('film.id'))
+
 
 class Address(Base):
     __tablename__ = 'address'
