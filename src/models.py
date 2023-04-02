3a2ca728ad384c984dad1210338122e89e727468
Marione Fiszman <marionef1985@gmail.com>
2023-04-02 13:20:21 +0000

    Almost ready

--- src/models.py
+++ src/models.py
@@ -8,23 +8,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    fisrt_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_card = Column(Integer, ForeignKey('character.id', 'planet.id'))
+    name = Column(String(250), ForeignKey('character.name', 'planet.name'))
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    image = Column(String(250))
+    name_planet = Column(String(250))
+    population  = Column(String(250))
+    terrain = Column(String(250))
+
+class Planet_Card(Base):
+    __tablename__ = 'planet_card'
+    id_planet_card = Column(Integer, ForeignKey('planet.id'))
+    image = Column(String(250))
+    name = Column(String(250), ForeignKey('planet.name_planet'))
+    description = Column(String(500))
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    image = Column(String(250))
+    name = Column(String(250))
+    gender = Column(String(250))
+    eye_color = Column(String(250))
+    hair_color   = COlumn(String(250))
+
+class Character_Card(Base):
+    character = relationship(Character)
+    id_character = Column(Integer, ForeignKey('character.id'))
+    #image = Column(String(250)) no se tendrian que poner porque salen de la relacion con la character, correcto?
+    #name = Column(String(250))
+    description = Column(String(500))
+
 
     def to_dict(self):
         return {}
