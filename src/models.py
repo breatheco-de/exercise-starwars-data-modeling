5e044c53f613abe1b96ef323c0e5950e2fceeb6e
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-04-29 20:07:45 +0000

    first commit, finished table just need to make sure everything that needs to be there is there

--- src/models.py
+++ src/models.py
@@ -1,29 +1,51 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class MyUser(Base):
+    __tablename__ = 'user'
+ 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    favorite = relationship('favorite')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Location(Base):
+    __tablename__ = 'location'
+ 
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False, unique=True)
+    type = Column(String(250), nullable=False)
+    dimension = Column(String(250), nullable=False)
+    favorite = relationship('favorite')
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    status = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    favorite = relationship('favorite')
+
+class Episode(Base):
+    __tablename__ = 'episode'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    status = Column(String(250), nullable=False)
+    favorite = relationship('favorite')
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    location_id = Column(Integer, ForeignKey('location.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    episode_id = Column(Integer, ForeignKey('episode.id'))
 
     def to_dict(self):
         return {}
