47780a80d18464f08d0cfd2ce71eb69e53cf7d71
JGC <juliogilca@gmail.com>
2023-07-03 19:01:59 +0000

    Finish modeling database

--- src/models.py
+++ src/models.py
@@ -23,7 +23,7 @@ class Planets(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    population = Column(Integer(10))
+    population = Column(String(10), nullable=False)
 
 class Characters(Base):
     __tablename__ = 'Characters'
@@ -34,10 +34,24 @@ class Characters(Base):
     gender = Column(String(50), nullable=False)
     hair_color = Column(String(50), nullable=False)
     eye_color = Column(String(50), nullable=False)
-    planet_born_id = Column(Integer(5), ForeignKey('Planets.id'))
+    planet_born_id = Column(Integer, ForeignKey('Planets.id'))
 
+class Favorits(Base):
+    __tablename__ = 'Favorits'
+    id = Column(Integer, primary_key=True)
+    id_Users = Column(Integer, ForeignKey('Users.id'))
+    id_Planets = Column(Integer, ForeignKey('Planets.id'))
+    id_Characters = Column(Integer, ForeignKey('Characters.id'))
 
-
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     usuario_id = Column(Integer, ForeignKey('usuario.id'))
     # Users = relationship(Usuario)
 
     def to_dict(self):
