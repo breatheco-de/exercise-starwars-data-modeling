f1864c78aeb9a110c188e4f1d62062904b903659
Gabriel Montaño <134933872+GabrielM20-1@users.noreply.github.com>
2023-10-18 19:05:16 +0000

    feed:borrador

--- src/models.py
+++ src/models.py
@@ -25,6 +25,51 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    users = relationship('User', backref='planeta')
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+    planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=False)
+    planeta = relationship("planeta", backref="users")
+    personajes = relationship("personajes", backref="user")
+
+class personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", backref="personajes")
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", backref="personajes")
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", backref="posts")
+    personajes = relationship("personajes", backref="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author = relationship("User", backref="comments")
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", backref="comments")
+
+
     def to_dict(self):
         return {}
 
