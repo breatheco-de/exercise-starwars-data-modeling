163a8e92d7d65a618e5065f0e1f9d0fc83293cd6
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-02-22 19:22:09 +0000

    update model

--- src/models.py
+++ src/models.py
@@ -10,36 +10,55 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username= Column(String(50),nullable=False)
+    email = Column(String(200), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
     name = Column(String(100), nullable=False)
-    surname = Column(String(100), nullable=False)
-    email = Column(String(200), nullable=False)
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    description = Column(String(1000), nullable=False)
+    population = Column(Integer, nullable=False)
 
 class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
-    description = Column(String(1000), nullable=False)
+    homeworld= Column(String(50), ForeignKey('planet.name'))
+    planet = relationship(Planet)
+
+class Film(Base):
+    __tablename__ = 'film'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    director = Column(String(50), nullable=False)
+    characters = Column(String(50),ForeignKey('character.name'))
+    character = relationship(Character)
+
+
+class Director(Base):
+    __tablename__ = 'director'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    surname = Column(String(100), nullable=False)
+    films = Column(String(50),ForeignKey('film.name'))
+    film = relationship(Film)
+    
 
 class Favorite(Base):
     __tablename__ = 'favorites'
     id = Column(Integer,primary_key=True)
     user_id = Column(Integer,ForeignKey('user.id'))
-    username = Column(String(50),nullable=False)
     user = relationship(User)
-    planet_id = Column(Integer,ForeignKey('planet.id'))
-    planet_name = Column(String(100), nullable=False)
-    planet = relationship(Planet)
-    char_id = Column(Integer,ForeignKey('character.id'))
-    char = Column(String(100), nullable=False)
-    planet = relationship(Character)
+    favorite_planets = Column(String(50),ForeignKey('planet.name'))
+    planets = relationship(Planet)
+    favorite_characters = Column(String(50),ForeignKey('character.name'))
+    character = relationship(Character)
+    favorite_films = Column(String(50),ForeignKey('film.name'))
+    film = relationship(Film)
+    favorite_directors = Column(String(50),ForeignKey('director.name'))
+    director = relationship(Director)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
