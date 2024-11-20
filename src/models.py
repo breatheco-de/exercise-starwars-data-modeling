7ff50fc3b876783ecbf09daf05c6e90ce6b62304
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-11-20 23:54:09 +0000

    Master

--- src/models.py
+++ src/models.py
@@ -7,26 +7,28 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Planet(db.Model):
+    __tablename__ = 'planet'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    popilation = db.Column(db.integer)
+    wather = db.Column(db.String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+
+
+    def __rep__(self):
+        return {Planet %r} % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+
+
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
